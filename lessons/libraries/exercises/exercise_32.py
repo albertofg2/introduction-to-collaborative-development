ebe2310d292984f898ee00baa90b83e3258fa565
albertofg2 <albertogol21@gmail.com>
2023-03-09 00:59:08 +0100

    libraries ej31

---
+++ lessons/libraries/exercises/exercise_32.py
@@ -0,0 +1,15 @@
+import scipy
+import matplotlib.pyplot as plt
+
+def splitter(:):
+
+    img = misc.face()
+
+    img_roja=
+    img_verde=
+    img_azul=
+
+    imagenes=[img_roja, img_verde, img_azul]
+
+
+    return imagenes
