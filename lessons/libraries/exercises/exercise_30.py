42bd4aec1fa5e54210f5e947078ed8dfdc81c111
albertofg2 <albertogol21@gmail.com>
2023-03-08 23:16:23 +0100

    libraries ej 30-31

--- lessons/libraries/exercises/exercise_30.py
+++ lessons/libraries/exercises/exercise_30.py
@@ -1,11 +1,4 @@
-import numpy as np
+from numpy import arange, pi, sin
 
-x=np.arange(0,2*PI,.1)
-
-
-
-
-
-
-Generate a range between `0` and `2 * pi` (you might need to import `pi` from a library) with a step of `0.1`. Store it in a variable named `x`.
-2. Generate, in one line, the images for all X values using the `sin` function. Do not use list comprehensions; you must pass the `x` array as an argument for the function. Store the result in a variable named `y`.
+x=arange(0,2*pi,.1)
+y=sin(x)
---
+++ lessons/libraries/exercises/exercise_31.py
@@ -0,0 +1,10 @@
+
+
+
+
+Steps:
+
+1. Compute the integral of the `sin` function in the `[0, pi / 2]` interval using two methods:
+    1. Riemann sum (store the result in the `riemann_integral` variable).
+    2. SciPy integration method (store the result in the `scipy_integral` variable).
+2. Measure the time it takes for each one of the processes to complete and display the **difference** on screen. You will need to use some **performance counter** method from the `time` library.
