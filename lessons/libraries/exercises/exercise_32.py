a1dd27f04c1079e03b87593a8982444c48eeb2d3
albertofg2 <albertogol21@gmail.com>
2023-03-09 16:30:35 +0100

    libraries try ej 32

--- lessons/libraries/exercises/exercise_32.py
+++ lessons/libraries/exercises/exercise_32.py
@@ -1,14 +1,21 @@
-import scipy
+from scipy import misc
 import matplotlib.pyplot as plt
 
-def splitter(:):
 
-    img = misc.face()
+def splitter():
+    # Cargar la imagen al programa
+    imagen = misc.face()
 
+    # Separar los canales en diferentes colores
+    for i in range (len(img(x))):
+       for i in range(len(img(y)))
+        img_roja=imagen[]
+    """
     img_roja=
     img_verde=
     img_azul=
-
+    """
+    # Union de las tres arrays en una sola lista
     imagenes=[img_roja, img_verde, img_azul]
 
 
