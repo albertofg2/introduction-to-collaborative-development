62e843f20b040abe144cfb523d9fc9d232a78aea
albertofg2 <albertogol21@gmail.com>
2023-03-09 18:31:01 +0100

    Update exercise_31.py

--- lessons/libraries/exercises/exercise_31.py
+++ lessons/libraries/exercises/exercise_31.py
@@ -1,23 +1,25 @@
-from numpy import sin, pi
-from scipy import quad
+import numpy as np
+from scipy import integrate
 import time
 
+a, b = 0,np.pi/2
 
-a, b = 0, pi/2
+# Por suma de Riemann
+start_time = time.perf_counter()
+n = 10000
+dx = (b-a)/n
+x=np.arange(a,b,dx)
+riemann_integral = sum(np.sin(x)*dx)
+time_riemann = time.perf_counter()-start_time
 
-# Por suma de Rieman
-start_time=time.perf_counter()
-n=1000
-△ x  =(b-a)/n
-integral_rieman=(∑ i = 1 n f ( x i ∗ )△ x )
-time_rieman=time.perf_counter()-start_time
-
-# Por scipy
-start_time=time.perf_counter()
-scipy_integral, scipy_error = quad(a,b)
-time_scipy=time.perf_counter()-start_time
+# Integral por scypi
+start_time = time.perf_counter()
+scipy_integral, scipy_error = integrate.quad(np.sin, a, b)
+time_scipy = time.perf_counter()-start_time
 
 # Resultados
-print('El tiempo de Rieman ha sido de',time_rieman)
-print('El tiempo de Rieman ha sido de',time_scipy)
-print('La diferencia de tiempos ha sido de',rieman_time-time_scipy)
+print('La integral por Riemann es:', riemann_integral)
+print('La integral por Scipy es:', scipy_integral)
+print('El tiempo de Riemann ha sido de', time_riemann)
+print('El tiempo de Scipy ha sido de', time_scipy)
+print('La diferencia de tiempos ha sido de', time_riemann-time_scipy)
