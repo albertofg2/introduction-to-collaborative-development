afce3f30bf2711fcec398cd105267fc999665873
albertofg2 <albertogol21@gmail.com>
2023-03-09 17:42:55 +0100

    ejercicio 32 primer intento

--- lessons/libraries/exercises/exercise_32.py
+++ lessons/libraries/exercises/exercise_32.py
@@ -1,20 +1,27 @@
-from scipy import misc
 import matplotlib.pyplot as plt
+import scipy
 
 
 def splitter():
     # Cargar la imagen al programa
-    imagen = misc.face()
+    img =  scipy.datasets.face()
 
     # Separar los canales en diferentes colores
-    for i in range (len(img(x))):
-       for i in range(len(img(y)))
-        img_roja=imagen[]
+    for i in range(len(img)):
+        for j in range(len(img[0])):
+            img_roja = img[i,j][0]
+            img_verde = img[i,j][1]
+            img_azul = img[i,j][2]
+
+
+
+
     """
-    img_roja=
-    img_verde=
-    img_azul=
+    img_roja = img[:,:,0]
+    img_verde = img[:,:,1]
+    img_azul = img[:,:,2]
     """
+
     # Union de las tres arrays en una sola lista
     imagenes=[img_roja, img_verde, img_azul]
 
